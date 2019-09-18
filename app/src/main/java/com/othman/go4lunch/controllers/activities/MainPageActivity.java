95fd4820d8e969e79d0f4b2052107796e34ae969
Othman <othman3492@gmail.com>
2019-09-18 18:42:16 +0200

    Update UI in Navigation Drawer, create workmates and restaurants items layouts, secure API keys

--- app/src/main/java/com/othman/go4lunch/controllers/activities/MainPageActivity.java
+++ app/src/main/java/com/othman/go4lunch/controllers/activities/MainPageActivity.java
@@ -1,6 +1,7 @@
 package com.othman.go4lunch.controllers.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,17 +11,27 @@ import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.othman.go4lunch.R;
 import com.othman.go4lunch.controllers.fragments.ListFragment;
 import com.othman.go4lunch.controllers.fragments.MapFragment;
 import com.othman.go4lunch.controllers.fragments.WorkmatesFragment;
+
+import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,7 +43,8 @@ public class MainPageActivity extends AppCompatActivity implements NavigationVie
     DrawerLayout drawerLayout;
     @BindView(R.id.main_page_toolbar)
     Toolbar mainToolbar;
-    @BindView(R.id.activity_main_page_nav_view) NavigationView navigationView;
+    @BindView(R.id.activity_main_page_nav_view)
+    NavigationView navigationView;
     @BindView(R.id.activity_main_page_bottom_nav_view)
     BottomNavigationView bottomNavigationView;
 
@@ -46,18 +58,22 @@ public class MainPageActivity extends AppCompatActivity implements NavigationVie
         setSupportActionBar(mainToolbar);
 
         // Configure UI
-        setMapFragmentOnStartActivity();
+        updateUIWhenCreating();
         navigationView.setNavigationItemSelectedListener(this);
         configureDrawerLayout();
         configureBottomNavigationView();
     }
 
-    // Display map by default when app starts
-    private void setMapFragmentOnStartActivity() {
-
-        Fragment fragment = new MapFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+    // Get data from current user
+    @Nullable
+    protected FirebaseUser getCurrentUser() {
+        return FirebaseAuth.getInstance().getCurrentUser();
     }
+
+    protected boolean isUserLogged() {
+        return (this.getCurrentUser() != null);
+    }
+
 
     // Configure menu in the toolbar
     @Override
@@ -97,13 +113,13 @@ public class MainPageActivity extends AppCompatActivity implements NavigationVie
 
             switch (menuItem.getItemId()) {
 
-                case R.id.bottom_map :
+                case R.id.bottom_map:
                     selectedFragment = new MapFragment();
                     break;
-                case R.id.bottom_list_view :
+                case R.id.bottom_list_view:
                     selectedFragment = new ListFragment();
                     break;
-                case R.id.bottom_workmates :
+                case R.id.bottom_workmates:
                     selectedFragment = new WorkmatesFragment();
             }
 
@@ -119,13 +135,13 @@ public class MainPageActivity extends AppCompatActivity implements NavigationVie
 
         switch (menuItem.getItemId()) {
 
-            case R.id.main_page_drawer_lunch :
-            break;
-            case R.id.main_page_drawer_settings :
-            break;
-            case R.id.main_page_drawer_logout :
+            case R.id.main_page_drawer_lunch:
+                break;
+            case R.id.main_page_drawer_settings:
+                break;
+            case R.id.main_page_drawer_logout:
                 signOutFromFirebase();
-            break;
+                break;
         }
 
 
@@ -133,11 +149,48 @@ public class MainPageActivity extends AppCompatActivity implements NavigationVie
     }
 
 
+    // Update UI when activity is creating
+    private void updateUIWhenCreating() {
+
+        // Display map fragment by default
+        Fragment fragment = new MapFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
+        // Configure views if current user is not null
+        if (getCurrentUser() != null) {
+
+            View header = navigationView.getHeaderView(0);
+            ImageView headerUserImage = header.findViewById(R.id.header_user_image);
+            TextView headerUserName = header.findViewById(R.id.header_user_name);
+            TextView headerUserEmail = header.findViewById(R.id.header_user_email);
+
+            // Get user profile picture from Firebase
+            if (getCurrentUser().getPhotoUrl() != null) {
+
+                Glide.with(this)
+                        .load(getCurrentUser().getPhotoUrl())
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(headerUserImage);
+            }
+
+            // Get username and email from Firebase, and update views
+            String username = TextUtils.isEmpty(getCurrentUser().getDisplayName()) ?
+                    getString(R.string.no_username_found) : this.getCurrentUser().getDisplayName();
+            String email = TextUtils.isEmpty(getCurrentUser().getEmail()) ?
+                    getString(R.string.no_email_found) : this.getCurrentUser().getEmail();
+
+            headerUserName.setText(username);
+            headerUserEmail.setText(email);
+        }
+
+    }
+
+
+    // Log out user and update UI
     private void signOutFromFirebase() {
 
         AuthUI.getInstance().signOut(this).addOnSuccessListener(this, this.updateUIAfterRESTRequestsCompleted());
     }
-
 
     private OnSuccessListener<Void> updateUIAfterRESTRequestsCompleted() {
 
